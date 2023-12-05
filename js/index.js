22b0701429ae90c3390642c536ac5f15565632d7
Niko Cremer <n.ko@posteo.de>
2023-12-05 12:01:18 +0100

    update

--- js/index.js
+++ js/index.js
@@ -1,3 +1,17 @@
 import "./game.js";
+import { fetchNewColor } from "./fetch.js";
 
 console.clear();
+
+// Check out the [`js/fetch.js`](./js/fetch.js) file:
+//There is a `fetchNewColor()` function which creates a random hex code and creates the correct url to fetch data from the Color API.
+
+// Your task is to implement the fetch functionality:
+
+// 1. Use the `fetch()` function to get data from the Color API.
+// 2. Use the `.json()` method to parse the body of the response and convert it into an object.
+// 3. Inspect the parsed object (e.g. by logging it to the console) and find the hex value and name of the closest named color.
+// 4. Call the `setColorToGuess()` function with the hex value and name of the closest named color.
+// 5. Guess colors! 🎉
+
+// Switch to the [`js/fetch.js`](./js/fetch.js) file and start coding!
